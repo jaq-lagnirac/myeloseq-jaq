2708423a7197047b9d792535adae6707091ce21a
Justin Caringal <jacfriday10th@gmail.com>
2023-06-29 14:24:56 -0500

    Initalize comparison-dev
    -Introduced Python script to compare whether the coverage in the
     MyeloseqHD JSON files matches the coverage in the BED files

---
+++ json_bed_comparison.py
@@ -0,0 +1,52 @@
+# Justin Caringal
+# Compares whether the coverage in the MyeloseqHD JSON files
+# matches the coverage in the BED files
+
+
+import os,sys
+import argparse
+import logging
+import json
+
+# - jaq
+from enum import Enum
+import time
+import statistics
+
+SCRIPT_PATH = os.path.abspath(__file__)
+FORMAT = '[%(asctime)s] %(levelname)s %(message)s'
+l = logging.getLogger()
+lh = logging.StreamHandler()
+lh.setFormatter(logging.Formatter(FORMAT))
+l.addHandler(lh)
+l.setLevel(logging.INFO)
+debug = l.debug; info = l.info; warning = l.warning; error = l.error
+
+DESCRIPTION = '''
+'''
+
+EPILOG = '''
+'''
+
+class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
+    argparse.RawDescriptionHelpFormatter):
+  pass
+parser = argparse.ArgumentParser(description=DESCRIPTION, epilog=EPILOG,
+  formatter_class=CustomFormatter)
+
+parser.add_argument('json')
+parser.add_argument('bed')
+parser.add_argument('-v', '--verbose',
+                    action='store_true',
+                    help='Set logging level to DEBUG')
+
+args = parser.parse_args()
+
+if args.verbose:
+  l.setLevel(logging.DEBUG)
+
+debug('%s begin', SCRIPT_PATH)
+
+
+
+debug('%s end', SCRIPT_PATH)
