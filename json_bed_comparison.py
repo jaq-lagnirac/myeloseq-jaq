bb62fb49fef6539d6645bd5541d3754d91b62237
Justin Caringal <jacfriday10th@gmail.com>
2023-06-29 15:45:07 -0500

    Comparing JSON and BED files

--- json_bed_comparison.py
+++ json_bed_comparison.py
@@ -3,15 +3,16 @@
 # matches the coverage in the BED files
 
 
-import os,sys
+import os
+import sys
 import argparse
 import logging
 import json
+import pandas as pd
+import intervaltree
 
-# - jaq
-from enum import Enum
-import time
-import statistics
+JSON_SUFFIX = '.report.json'
+BED_SUFFIX = '.qc-coverage-region-1_full_res.bed'
 
 SCRIPT_PATH = os.path.abspath(__file__)
 FORMAT = '[%(asctime)s] %(levelname)s %(message)s'
@@ -34,8 +35,8 @@ class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
 parser = argparse.ArgumentParser(description=DESCRIPTION, epilog=EPILOG,
   formatter_class=CustomFormatter)
 
-parser.add_argument('json')
-parser.add_argument('bed')
+parser.add_argument('directory',
+                    help='main directory for comparison')
 parser.add_argument('-v', '--verbose',
                     action='store_true',
                     help='Set logging level to DEBUG')
@@ -47,6 +48,34 @@ if args.verbose:
 
 debug('%s begin', SCRIPT_PATH)
 
+for directory_name in os.listdir(args.directory):
+  json_path = os.path.join(args.directory,
+                           directory_name,
+                           f'{directory_name}{JSON_SUFFIX}')
+  bed_path = os.path.join(args.directory,
+                          directory_name,
+                          f'{directory_name}{BED_SUFFIX}')
+  
+  with open(json_path) as jp:
+    json_file = json.loads(jp.read())
+  
+  # print(json_path)
 
+  df = pd.read_csv(bed_path,
+                   sep='\t',
+                   header=None)
+  try:
+    tier13_columns = json_file['VARIANTS']['TIER1-3']['columns']
+    tier13_data = json_file['VARIANTS']['TIER1-3']['data']
+    # print(tier13_columns)
+  except:
+    error(f'{json_path} : Column does not exist')
+
+  for entry in tier13_data:
+    chrom = entry[tier13_columns.index('chrom')]
+    pos = entry[tier13_columns.index('pos')]
+    ref = entry[tier13_columns.index('ref')]
+    alt = entry[tier13_columns.index('alt')]
+  print(f'{chrom}\t{pos}\t{ref}\t{alt}')
 
 debug('%s end', SCRIPT_PATH)
