edac5f5e986f745d6b18289de87c4c2243590a7b
Justin Caringal <jacfriday10th@gmail.com>
2023-06-30 12:26:06 -0500

    IntervalTree implementation
    -Successfully implemented IntervalTree to process BED files per
     subdirectory

--- json_bed_comparison.py
+++ json_bed_comparison.py
@@ -9,7 +9,7 @@ import argparse
 import logging
 import json
 import pandas as pd
-import intervaltree
+from intervaltree import Interval, IntervalTree
 
 JSON_SUFFIX = '.report.json'
 BED_SUFFIX = '.qc-coverage-region-1_full_res.bed'
@@ -46,9 +46,34 @@ args = parser.parse_args()
 if args.verbose:
   l.setLevel(logging.DEBUG)
 
+
+
+def process_json_entry(pos, ref, alt):
+  # SNV
+  if (len(ref) == 1) and (len(ref) == 1):
+    start = pos
+    end = pos
+  # DELETION
+  elif (len(ref) > 1) and (len(alt) == 1):
+    start = pos + 1
+    end = pos + len(ref) - 1
+  # INSERTION
+  elif (len(ref) == 1) and (len(alt) > 1):
+    start = pos
+    end = pos + 1
+  else:
+    start = pos
+    end = start + len(alt) - 1
+
+  return start, end
+
+
+
 debug('%s begin', SCRIPT_PATH)
 
+
 for directory_name in os.listdir(args.directory):
+  # Generate file paths
   json_path = os.path.join(args.directory,
                            directory_name,
                            f'{directory_name}{JSON_SUFFIX}')
@@ -56,26 +81,50 @@ for directory_name in os.listdir(args.directory):
                           directory_name,
                           f'{directory_name}{BED_SUFFIX}')
   
+  
+  # Process BED file
+  df = pd.read_csv(bed_path,
+                   sep = '\t',
+                   names = ['chrom',
+                            'start',
+                            'end',
+                            'coverage'])
+  
+  # Create interval tree from pandas dataframe
+  bed_tree = IntervalTree()
+  for index, row in df.iterrows():
+    bed_tree[row['start']:row['end']] = row['coverage']
+
+  #for obj in bed_tree:
+  #  print(obj)
+  #  print(f"{row['chrom']}\t{row['start']}\t{row['end']}\t{row['coverage']}")
+
+
+  # Process JSON file
+  
   with open(json_path) as jp:
     json_file = json.loads(jp.read())
   
-  # print(json_path)
-
-  df = pd.read_csv(bed_path,
-                   sep='\t',
-                   header=None)
   try:
     tier13_columns = json_file['VARIANTS']['TIER1-3']['columns']
     tier13_data = json_file['VARIANTS']['TIER1-3']['data']
     # print(tier13_columns)
+    info(f'Accessing file: {json_path}')
   except:
-    error(f'{json_path} : Column does not exist')
+    error(f'Tier 1-3 columns do not exist: {json_path}')
+    continue
+
+
+  # Comparing files
 
   for entry in tier13_data:
     chrom = entry[tier13_columns.index('chrom')]
-    pos = entry[tier13_columns.index('pos')]
+    pos = int(entry[tier13_columns.index('pos')])
     ref = entry[tier13_columns.index('ref')]
     alt = entry[tier13_columns.index('alt')]
-  print(f'{chrom}\t{pos}\t{ref}\t{alt}')
+    cov = entry[tier13_columns.index('coverage')]
+    
+    start, end = process_json_entry(pos, ref, alt)
+
 
 debug('%s end', SCRIPT_PATH)
